7f39c9f2264976d1b58d2dd83692e12140b84078
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-07-05 18:04:11 -0400

    styles_started

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import styles from '../styles/HomePage.module.scss';
+
 const HomePage = () => (
-  <div>
-    HomePage
+  <div className={styles.home}>
+    <div className={styles.output}>
+
+    </div>
+    <div className={styles.mainControls}>
+      
+    </div>
+    <div className={styles.ratioButton}>
+      ratio
+    </div>
   </div>
 );
 
